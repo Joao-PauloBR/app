7f02f501191b4c776412c3ccad7ace69a86d0c5b
João Paulo Souza Bernucio <joaobernucio@proton.me>
2024-09-15 20:08:48 -0300

    Class 1 completed

---
+++ arrays_objects.js
@@ -0,0 +1,23 @@
+// Arrays, Objetos
+
+let metas = ["joão?", "alô"];
+
+console.log(metas[1] + ", " + metas[0]);
+
+// -----
+
+let objeto = {
+    value: "Ler um livro por mês",
+    checked: false
+};
+
+console.log(objeto.value + ": " + objeto.checked);
+
+// -----
+
+// Arrow function
+const criarMeta = () => {};
+
+// Named Function
+function criarMeta() {};
+// Como estão com o mesmo nome, se eu tentar rodar o código, irá dar erro. Mas como o objetivo era mostrar que são 
--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-// Arrays, Objetos
-
-let metas = ["joão?", "alô"];
-
-console.log(metas[1] + ", " + metas[0]);
-
-// -----
-
-let objeto = {
-    value: "Ler um livro por mês",
-    checked: false
+function start(){
+    console.log("Utilizando function");
 };
 
-console.log(objeto.value + ": " + objeto.checked);
+const first = () => {
+    console.log("Utilizando arrow function")
+};
 
-// -----
-
-// Arrow function
-const criarMeta = () => {};
-
-// Named Function
-function criarMeta() {};
-// Como estão com o mesmo nome, se eu tentar rodar o código, irá dar erro. Mas como o objetivo era mostrar que são 
+start();
+first();
